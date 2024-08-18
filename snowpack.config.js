ee022d4e9f287c53c8db1fa0a22c3c49119aabb8
Batuhan Eyüboğlu <batuhan.eyuboglu@outlook.com>
2024-08-18 18:17:55 +0300

    Test config

---
+++ snowpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    optimize: {
+      bundle: true,
+      minify: true,
+      target: 'es2018',
+    },
+    mount: {
+      /* ... */
+    },
+    plugins: [
+      /* ... */
+    ],
+    packageOptions: {
+      /* ... */
+    },
+    devOptions: {
+      /* ... */
+    },
+    buildOptions: {
+      /* ... */
+    },
+  };
